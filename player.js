fc580c214fc8644aecb11655da3029f41b48e028
Gagandeep Dass Kaur <gdass@admira.com>
2022-11-11 13:09:43 +0100

    update

--- player.js
+++ player.js
@@ -12,7 +12,7 @@ var socket = io.connect("http://localhost:3000", { forceNew: true });
 
 socket.on('parking', function (data) {
 
-    console.log("Data:-->"+data+"<--")
+    //console.log("Data:-->"+data+"<--")
     
 
     /*
