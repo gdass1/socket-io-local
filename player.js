b0a38840211ff644a4906b601ff355f33bb6bf32
Gagandeep Dass Kaur <gdass@admira.com>
2022-11-11 14:53:55 +0100

    update

--- player.js
+++ player.js
@@ -1,5 +1,4 @@
-
-
+console.log("hola , he entrado en el script")
 let monestirStatus1 = document.getElementById("parking-monestir-status-1");
 let monestirStatus2 = document.getElementById("parking-monestir-status-2");
 let monestirStatus3 = document.getElementById("parking-monestir-status-3");
@@ -10,21 +9,32 @@ let cremalleraStatus3 = document.getElementById("parking-cremallera-status-3");
 
 var socket = io.connect("http://localhost:3000", { forceNew: true });
 
+socket.on('connect', function() {
+    socket.on('parking', function (data) {
+
+        //console.log("Data:-->"+data+"<--")
+    
+        if(data.parking) {
+            console.log("Parking: -->"+data.parking+"<--")
+            if(data.parking == 'monestir' || data.parking == 'Monestir') {
+                monestirStatus1.innerHTML = data.message
+                monestirStatus2.innerHTML = data.message
+                monestirStatus3.innerHTML = data.message
+            } else {
+                data.parking == 'Cremallera';
+                cremalleraStatus1.innerHTML = data.message
+                cremalleraStatus2.innerHTML = data.message
+                cremalleraStatus3.innerHTML = data.message
+            }
+        }
+        
+    })
+})
+
+/*
 socket.on('parking', function (data) {
 
     //console.log("Data:-->"+data+"<--")
-    
-
-    /*
-    if(data.message) {
-        console.log("Message: -->"+data.message+"<--")
-        monestirStatus1.innerHTML = data.message
-        monestirStatus2.innerHTML = data.message
-        monestirStatus3.innerHTML = data.message
-        cremalleraStatus1.innerHTML = data.message
-        cremalleraStatus2.innerHTML = data.message
-        cremalleraStatus3.innerHTML = data.message
-    }*/
 
     if(data.parking) {
         console.log("Parking: -->"+data.parking+"<--")
@@ -41,3 +51,4 @@ socket.on('parking', function (data) {
     }
     
 })
+*/
