435a68ff55e08f0dc86727e272acc3982ce19231
Gagandeep Dass Kaur <gdass@admira.com>
2022-11-11 12:33:29 +0100

    switch between parkings

--- player.js
+++ player.js
@@ -12,8 +12,32 @@ var socket = io.connect("http://localhost:3000", { forceNew: true });
 
 socket.on('parking', function (data) {
 
-    console.log(data)
+    console.log("Data:-->"+data+"<--")
     
-    monestirStatus1.innerHTML = data
-    cremalleraStatus1.innerHTML = data
+
+    /*
+    if(data.message) {
+        console.log("Message: -->"+data.message+"<--")
+        monestirStatus1.innerHTML = data.message
+        monestirStatus2.innerHTML = data.message
+        monestirStatus3.innerHTML = data.message
+        cremalleraStatus1.innerHTML = data.message
+        cremalleraStatus2.innerHTML = data.message
+        cremalleraStatus3.innerHTML = data.message
+    }*/
+
+    if(data.parking) {
+        console.log("Parking: -->"+data.parking+"<--")
+        if(data.parking == 'monestir' || data.parking == 'Monestir') {
+            monestirStatus1.innerHTML = data.message
+            monestirStatus2.innerHTML = data.message
+            monestirStatus3.innerHTML = data.message
+        } else {
+            data.parking == 'Cremallera';
+            cremalleraStatus1.innerHTML = data.message
+            cremalleraStatus2.innerHTML = data.message
+            cremalleraStatus3.innerHTML = data.message
+        }
+    }
+    
 })
